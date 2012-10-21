2e811ad7c12f870aed4b72a910389ad631f30f76
Anders Furseth <anders.furseth@gmail.com>
2012-10-21 17:25:45 +0200

    inferior-lisp-program set to clisp

--- init.el
+++ init.el
@@ -9,3 +9,6 @@
 ;; Add ~/bin to path
 (setenv "PATH" (concat (getenv "PATH") ":/Users/anders/bin"))
 (setq exec-path (append exec-path '("/Users/anders/bin")))
+
+;; Common lisp stuff
+;(setq inferior-lisp-program "/usr/local/bin/clisp")
