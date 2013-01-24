f24952171f2b5a9157b812c69b55fb14e45a89cf
Anders Furseth <anders.furseth@gmail.com>
2013-01-25 00:57:53 +0100

    Bindings for expand-region and multiple-cursors.

--- config/bindings.el
+++ config/bindings.el
@@ -1,5 +1,11 @@
-;; Place your bindings here.
-
-;; For example:
-(define-key global-map (kbd "C-+") 'text-scale-increase)
+;; Scaling of buffer
+(define-key global-map (kbd "C-=") 'text-scale-increase)
 (define-key global-map (kbd "C--") 'text-scale-decrease)
+
+;; expand-region
+(global-set-key (kbd "C-|") 'er/expand-region)
+
+;; multiple-cursors
+(global-set-key (kbd "C->") 'mc/mark-next-like-this)
+(global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
+(global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this)
