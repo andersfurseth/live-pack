ef11a2a56c48a24b3fcfc5ae0b0d3fd1a0bc9fa8
Anders Furseth <anders.furseth@gmail.com>
2013-01-25 18:38:23 +0100

    Misc bindings.

--- config/bindings.el
+++ config/bindings.el
@@ -9,3 +9,17 @@
 (global-set-key (kbd "C->") 'mc/mark-next-like-this)
 (global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
 (global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this)
+
+;; window-switching-mode
+(win-switch-set-keys '("w") 'up)
+(win-switch-set-keys '("s") 'down)
+(win-switch-set-keys '("a") 'left)
+(win-switch-set-keys '("d") 'right)
+(win-switch-set-keys '("o") 'next-window)
+(win-switch-set-keys '("p") 'previous-window)
+(win-switch-set-keys '("W") 'enlarge-vertically)
+(win-switch-set-keys '("S") 'shrink-vertically)
+(win-switch-set-keys '("A") 'shrink-horizontally)
+(win-switch-set-keys '("D") 'enlarge-horizontally)
+
+(setq win-switch-idle-time 1.2)
